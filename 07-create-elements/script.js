9d43dc5090715317429d88a9ca34ef71e9932b68
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-15 18:32:57 +0100

    Finished lesson on creating elements

---
+++ 07-create-elements/script.js
@@ -0,0 +1,9 @@
+// =========== 07 Creating Elements ============ //
+
+const div = document.createElement('div');
+div.className = 'my-element';
+div.id = 'my-element';
+div.setAttribute('title', 'My Element');
+const text = document.createTextNode('My Element');
+div.appendChild(text);
+document.body.appendChild(div);
