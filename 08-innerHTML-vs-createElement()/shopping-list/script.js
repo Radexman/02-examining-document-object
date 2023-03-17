756df909a4126ca1e9469018912e3a8ce6768326
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-17 14:25:44 +0100

    17.03

--- 08-innerHTML-vs-createElement()/shopping-list/script.js
+++ 08-innerHTML-vs-createElement()/shopping-list/script.js
@@ -1,42 +1,45 @@
 // ======== 08 innerHTML vs createElement ======= //
 
+/* <li>
+Apples
+<button class="remove-item btn-link text-red">
+<i class="fa-solid fa-xmark"></i>
+</button>
+</li> */
+
+const itemsList = document.querySelector('#item-list');
+
 // Quick & Dirty
 
-// function createListItem(item) {
-// 	const li = document.createElement('li');
-// 	li.innerHTML = `${item}
-// 					<button class="remove-item btn-link text-red">
-// 						<i class="fa-solid fa-xmark"></i>
-// 					</button>`;
-// 	document.querySelector('.items').appendChild(li);
-// }
+const createItemOne = (item) => {
+	const li = document.createElement('li');
+	li.innerHTML = `${item}
+		<button class="remove-item btn-link text-red">
+		<i class="fa-solid fa-xmark"></i>
+		</button>
+	`;
+	itemsList.appendChild(li);
+};
 
-// createListItem('Eggs');
+createItemOne('Peanut butter');
 
 // Clean & Performant
 
-// <li>
-// 	Orange Juice
-// 	<button class="remove-item btn-link text-red">
-// 		<i class="fa-solid fa-xmark"></i>
-// 	</button>
-// </li>;
-
-function createNewLi(item) {
+const createItemTwo = (item) => {
 	const li = document.createElement('li');
 	const liText = document.createTextNode(item);
-	li.appendChild(liText);
 
 	const button = document.createElement('button');
 	button.className = 'remove-item btn-link text-red';
 
 	const icon = document.createElement('i');
 	icon.className = 'fa-solid fa-xmark';
+
+	li.appendChild(liText);
 	button.appendChild(icon);
 	li.appendChild(button);
+	itemsList.appendChild(li);
+};
 
-	document.querySelector('.items').appendChild(li);
-}
-
-createNewLi('Bread');
-createNewLi('Eggs');
+createItemTwo('Eggs');
+createItemTwo('Orange juice');
