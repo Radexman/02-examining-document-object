d0e82065ccdd050b2744910d3db6590d0b487ff0
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-17 13:59:10 +0100

    17.03

--- 07-create-elements/script.js
+++ 07-create-elements/script.js
@@ -1,9 +1,9 @@
 // =========== 07 Creating Elements ============ //
-
 const div = document.createElement('div');
 div.className = 'my-element';
 div.id = 'my-element';
 div.setAttribute('title', 'My Element');
-const text = document.createTextNode('My Element');
+const text = document.createTextNode('My Div Element');
 div.appendChild(text);
+
 document.body.appendChild(div);
